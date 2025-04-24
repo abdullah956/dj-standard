df112f4997520aa5194023d42d7f26e8cec9aae0
abdullah956 <abdullaharshed956@gmail.com>
2025-04-24 16:34:03 +0500

    add

--- qr/urls.py
+++ qr/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import add_certificate, certificate_list, search_certificate
 
 urlpatterns = [
-    path('add/', add_certificate, name='add_certificate'),
+    path('', add_certificate, name='add_certificate'),
     path('certificates/', certificate_list, name='certificate_list'),
     path('search/', search_certificate, name='search_certificate'),
 ]

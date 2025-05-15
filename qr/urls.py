8ec851866b1baaff0db27cd10bb9d5997928274d
abdullah956 <abdullaharshed956@gmail.com>
2025-05-15 18:40:38 +0500

    .

--- qr/urls.py
+++ qr/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import add_certificate, certificate_list, search_certificate
+from .views import add_certificate,logout_view, certificate_list, search_certificate,login_view
 
 urlpatterns = [
-    path('', add_certificate, name='add_certificate'),
-    path('certificates/', certificate_list, name='certificate_list'),
+    path('add/', add_certificate, name='add_certificate'),
+    path('', certificate_list, name='certificate_list'),
     path('search/', search_certificate, name='search_certificate'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
--- qr/views.py
+++ qr/views.py
@@ -12,6 +12,9 @@ def add_certificate(request):
         form = CertificationCardForm()
     return render(request, 'add_certificate.html', {'form': form})
 
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def certificate_list(request):
     certificates = CertificationCard.objects.all().order_by('-created_at')
     return render(request, 'certificate_list.html', {'certificates': certificates})
@@ -28,3 +31,22 @@ def search_certificate(request):
         except CertificationCard.DoesNotExist:
             certificate = None
     return render(request, 'search_certificate.html', {'certificate': certificate, 'searched': searched})
+
+
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('certificate_list')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
